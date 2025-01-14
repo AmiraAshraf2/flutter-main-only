100ac7e753cd318d11d06eb236804030f3e6a10b
AmiraAshraf2 <164785782+AmiraAshraf2@users.noreply.github.com>
2025-01-14 16:38:41 +0200

    Update news_services.dart

--- news_app/lib/services/news_services.dart
+++ news_app/lib/services/news_services.dart
@@ -7,7 +7,7 @@ class NewsServices {
   Future<List<ArticleModel>> getGeneralNews({required String category}) async {
     try {
       var response = await dio.get(
-          'https://newsapi.org/v2/top-headlines?category=$category&apiKey=c4eb45cf91a64291a39a6f8cef0accdf');
+          'https://newsapi.org/v2/top-headlines?category=$category&apiKey=');
       Map<String, dynamic> jesonData = response.data;
       List<dynamic> articles = jesonData['articles'];
       List<ArticleModel> articlesList = [];
